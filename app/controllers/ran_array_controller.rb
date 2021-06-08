fa7cf03d35d840f43e71aee1f08024b5ede17a70
Alexis Franco <alexisfrancolopez@gmail.com>
2021-06-08 09:22:20 -0500

    Code refactor

--- app/controllers/ran_array_controller.rb
+++ app/controllers/ran_array_controller.rb
@@ -1,10 +1,6 @@
 class RanArrayController < ApplicationController
   def index
-    arrays = RanArray.all
-    @response = arrays
-    @status = 200
-
-    render json: @response, serializer: RanArrayController, status: @status
+    render json: RanArray.all, status: 200
   end
 
   def create
@@ -13,26 +9,24 @@ class RanArrayController < ApplicationController
     if qty >= 10 && qty <= 30
       array = (0..30).to_a.sample(qty)
       new_array = RanArray.create! :number => array
-      @response = new_array
-      @status = 200
+      response = new_array
+      status = 200
     else
-      @response = { message: 'El número de elementos debe estar entre 10 y 30' }
-      @status = 400
+      response = { message: 'El número de elementos debe estar entre 10 y 30' }
+      status = 400
     end
 
-    render json: @response, serializer: RanArrayController, status: @status
+    render json: response, status: status
   end
 
   def show
-    arrays = RanArray.find(params[:id]).number.sort.slice_when  do |prev, curr|
+    array = RanArray.find(params[:id])
+    arrays = array.number.sort.slice_when  do |prev, curr|
       curr != prev.next
     end
-    ran_array = RanArray.new
-    return_array = ran_array.range(arrays)
 
-    @response = "El mayor rango del arreglo es: #{return_array}"
-    @status = 200
-    
-    render json: @response, serializer: RanArrayController, status: @status
+    ran_array = RanArray.new
+
+    render json: ran_array.range(arrays), status: 200
   end
 end
